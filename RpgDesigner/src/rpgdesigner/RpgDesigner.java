ef0e64dce6c86d73e39612765e8695db31dde736
james <james@holyphoenix>
2012-01-18 14:51:04 -0600

    Initial File Upload

---
+++ RpgDesigner/src/rpgdesigner/RpgDesigner.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rpgdesigner;
+
+/**
+ *
+ * @author james
+ */
+public class RpgDesigner {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
