78d7baaf9177d2311ee6637543c3e002eaf0465d
james <james@holyphoenix>
2012-01-23 18:41:43 -0600

    Reverting back to changeset before changes used as example in class to explain mercurial and google code.

--- RpgDesigner/src/rpgdesigner/Tile.java
+++ RpgDesigner/src/rpgdesigner/Tile.java
@@ -1,12 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package rpgdesigner;
-
-import java.awt.Image;
 
 /**
  *
  * @author james
  */
 class Tile {
-    private Image image;
     
 }
