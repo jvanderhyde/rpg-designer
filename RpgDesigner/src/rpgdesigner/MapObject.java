796b20960c2a955a2bcffb70a15b1d6dc6303e62
james <james@HolyPhoenix>
2012-04-18 21:35:48 -0500

    A better implementation of blocks.  They are still quite off in the playtest though, but at least they don't break it anymore.

---
+++ RpgDesigner/src/rpgdesigner/MapObject.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rpgdesigner;
+
+import java.awt.Image;
+import org.newdawn.slick.SlickException;
+
+/**
+ *
+ * @author Fran
+ */
+public interface MapObject {
+    
+    public void setLocation(int x, int y);
+    public Image getImage();
+    public float getLocY();
+    public float getLocX();
+    public int getTile();
+    public void setTile(int tileNumber);
+    public org.newdawn.slick.Image getSlickImage() throws SlickException ;
+}
